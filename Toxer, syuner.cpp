80a4fa6c8df10dacb1cb9d4446bd811e5cbb19f5
badalyang <126382208+badalyang@users.noreply.github.com>
2024-10-29 14:49:15 +0400

    Update Toxer-syuner.cpp

--- Toxer, syuner.cpp
+++ Toxer, syuner.cpp
@@ -2,44 +2,81 @@
 #include <string>
 using namespace std;
 
-int main() {
-    int mtx[100][100];  
-    string str;
-    cout << "Enter the sentence: ";
-    getline(cin, str); 
-
+void encrypt(const string& str, int key) {
+    int mtx[100][100];
     int count = str.length();
-    cout << "The length of the sentence is: " << count << endl;
-
-    int key;
-    cout << "Enter the number: ";
-    cin >> key;
-
     int m = count / key;
     if (count % key != 0) {
-        m += 1;  
+        m += 1;
     }
 
-    
     int index = 0;
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < key; j++) {
             if (index < count) {
-                mtx[i][j] = str[index];  
+                mtx[i][j] = str[index];
             } else {
-                mtx[i][j] = ' ';  
+                mtx[i][j] = ' '; 
             }
             index++;
         }
     }
 
-    cout << "Printing the matrix column by column: " << endl;
-    for (int j = 0; j < key; j++) {  
-        for (int i = 0; i < m; i++) { 
-            cout << (char)mtx[i][j];  /
+    cout << "Encrypted message: ";
+    for (int j = 0; j < key; j++) {
+        for (int i = 0; i < m; i++) {
+            cout << (char)mtx[i][j];
         }
-        cout << endl;  
     }
+    cout << endl;
+}
+
+void decrypt(const string& encrypted, int key) {
+    int mtx[100][100];
+    int count = encrypted.length();
+    int m = count / key;
+    if (count % key != 0) {
+        m += 1;
+    }
+
+    int index = 0;
+    for (int j = 0; j < key; j++) {
+        for (int i = 0; i < m; i++) {
+            if (index < count) {
+                mtx[i][j] = encrypted[index];
+            } else {
+                mtx[i][j] = ' ';
+            }
+            index++;
+        }
+    }
+
+    cout << "Decrypted message: ";
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < key; j++) {
+            cout << (char)mtx[i][j];
+        }
+    }
+    cout << endl;
+}
+
+int main() {
+    string str;
+    cout << "Enter the sentence to encrypt: ";
+    getline(cin, str);
+
+    int key;
+    cout << "Enter the key number: ";
+    cin >> key;
+
+    encrypt(str, key);
+
+    string encrypted;
+    cout << "Enter the encrypted message to decrypt: ";
+    cin.ignore();
+    getline(cin, encrypted);
+
+    decrypt(encrypted, key);
 
     return 0;
 }
